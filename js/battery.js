e85d7ffe71e27234c41063e8088ec6d8d7d05549
Matias <caicedomatias04@gmail.com>
2025-04-01 21:01:40 -0400

    Add files via upload

---
+++ js/battery.js
@@ -0,0 +1,33 @@
+
+const chargeStatus = document.querySelector('#battery dd:nth-of-type(1)');
+const chargeLevel = document.querySelector('#battery dd:nth-of-type(2) output');
+const chargeMeter = document.querySelector('#battery dd:nth-of-type(2) progress');
+
+
+const batteryImage = document.createElement("img");
+batteryImage.alt = "Battery Status Image";
+document.body.appendChild(batteryImage);
+
+
+function updateBatteryStatus(battery) {
+    console.log(battery);
+
+   
+    chargeStatus.textContent = battery.charging ? "Charging..." : "Discharging";
+
+   
+    let batteryPercentage = Math.round(battery.level * 100);
+    chargeLevel.textContent = batteryPercentage + "%";
+    chargeMeter.value = batteryPercentage;
+
+    
+    batteryImage.src = `https://robohash.org/${batteryPercentage}.png`;
+}
+
+navigator.getBattery().then(battery => {
+    updateBatteryStatus(battery);
+
+    
+    battery.addEventListener("chargingchange", () => updateBatteryStatus(battery));
+    battery.addEventListener("levelchange", () => updateBatteryStatus(battery));
+});
